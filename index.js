b60826026c3da762937d71513c73f4149a5f7136
adson vinicius <viniciusadson10@gmail.com>
2025-04-04 19:17:01 -0300

    Atualização da tabela e do JSON

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const formulario = document.getElementById('formulario');
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let nome = document.getElementById('nome').value;
-    let email = document.getElementById('email').value;
-    let dataentrada = document.getElementById('dataEntrada').value;
-    let datasaida = document.getElementById('dataSaida').value;
-    let obs = document.getElementById('obs').value;
-    let adultos = document.getElementById('adultos').value;
-    let criancas = document.getElementById('criancas').value;
+    let nome = document.getElementById('nome').value.trim();
+    let email = document.getElementById('email').value.trim();
+    let dataentrada = document.getElementById('dataEntrada').value.trim();
+    let datasaida = document.getElementById('dataSaida').value.trim();
+    let obs = document.getElementById('obs').value.trim();
+    let adultos = document.getElementById('adultos').value.trim();
+    let criancas = document.getElementById('criancas').value.trim();
 
     let dados = {
         nome: nome,
@@ -29,34 +29,15 @@ formulario.addEventListener('submit', (e) => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(dados)
+
     })
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error));
 
     localStorage.setItem('dados', JSON.stringify(dados));
-    alert("Deu certo!")
     window.location.href = 'tabela.html';
 
     var form = document.getElementById("formulario");
     form.reset();
 })};
-
-function tabela(){
-const table = document.getElementById('table');
-
-const dados = JSON.parse(localStorage.getItem('dados'));
-
-const linha = document.createElement('tr');
-linha.innerHTML = `
-    <td>${dados.nome}</td>
-    <td>${dados.email}</td>
-    <td>${dados.dataentrada}</td>
-    <td>${dados.datasaida}</td>
-    <td>${dados.obs}</td>
-    <td>${dados.adultos}</td>
-    <td>${dados.criancas}</td>
-`;
-
-table.appendChild(linha);
-}
